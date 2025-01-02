53a2695aad1ddb8248be1956607a2a50e64760c4
Romel Campbell <romelcampbell@gmail.com>
2025-01-02 03:47:51 -0500

    WIP

--- ipfs-js-101/main.js
+++ ipfs-js-101/main.js
@@ -1,10 +1,23 @@
-const ipfs = require('ipfs');
-console.log('Imported IPFS');
-const node = new ipfs();
-console.log('Starting server');
+// const ipfs = require('ipfs');
+// console.log('Imported IPFS');
+// const node = new ipfs();
+// console.log('Starting server');
 
-node.on('ready', async () => {
-    console.log('Server started');
-    const version = await node.version
-    console.log('IPFS Version: ' + version);
-});
+// node.on('ready', async () => {
+//     console.log('Server started');
+//     const version = await node.version
+//     console.log('IPFS Version: ' + version);
+// });
+
+import * as ipfs from 'ipfs-core';
+console.log("Imported IPFS");
+ 
+async function main() {
+    const node = await ipfs.create();
+    console.log ("Starting server");
+    console.log("Server started");
+    const version = await node.version();
+    console.log(version.version);
+}
+ 
+main();
