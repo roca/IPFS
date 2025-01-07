f11b8ae9bb8ee1910651ee4a248ffd2821981bea
Romel Campbell <romelcampbell@gmail.com>
2025-01-07 03:51:52 -0500

    WIP: index.js

---
+++ udemy/ipfs-networking/unbox/public/index.js
@@ -0,0 +1,8 @@
+/* global location */
+'use strict'
+
+const IPFS = require('IPFS')
+
+// node
+const $nodeId = document.querySelector('.node-id')
+const $nodeAddress = document.querySelector('.node-addresses')
